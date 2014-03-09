3b5901f6256b62d6f81eb3d14b2b58eb8673d481
Andrelug <pamasway@gmail.com>
2014-03-09 04:44:39 -0300

    Local sigin/signup completed

--- config/passport.js
+++ config/passport.js
@@ -14,14 +14,14 @@ module.exports = function (passport) {
     // passport needs ability to serialize and unserialize users out of session
 
     // used to serialize the user for the session
-    passport.serializeUser(function (Users, done) {
-        done(null, Users.id);
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
     });
 
     // used to deserialize the user
     passport.deserializeUser(function (id, done) {
-        Users.findById(id, function (err, Users) {
-            done(err, Users);
+        Users.findById(id, function (err, users) {
+            done(err, users);
         });
     });
 
@@ -33,14 +33,14 @@ module.exports = function (passport) {
 
     passport.use('local-signup', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
-        usernameField: 'loginName',
+        usernameField: 'email',
         passwordField: 'password',
         passReqToCallback: true // allows us to pass back the entire request to the callback
     },
-    function (req, loginName, password, done) {
+    function (req, email, password, done) {
         // Get the other parameters
-        var email = req.body.email,
-            gender = req.body.gender,
+        var gender = req.body.gender,
+            loginName = req.body.loginName,
             firstName = req.body.firstName;
        
 
@@ -64,7 +64,7 @@ module.exports = function (passport) {
                             return done(err);
 
                         if (login) {
-                            return done(null, false, req.flash('signupMessage', 'That username is already taken.'), {email: email, loginName: loginName, gender: gender, firstName: firstName});
+                            return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
                         } else{
                             // if there is no user with that email or username
                             // create the user
@@ -92,6 +92,41 @@ module.exports = function (passport) {
 
             });
 
+        });
+
+    }));
+
+    // =========================================================================
+    // LOCAL LOGIN =============================================================
+    // =========================================================================
+	// we are using named strategies since we have one for login and one for signup
+	// by default, if there was no name, it would just be called 'local'
+
+    passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) { // callback with email and password from our form
+
+		// find a user whose email is the same as the forms email
+		// we are checking to see if the user trying to login already exists
+        Users.findOne({ 'email' :  email }, function(err, user) {
+            // if there are any errors, return the error before anything else
+            if (err)
+                return done(err);
+
+            // if no user is found, return the message
+            if (!user)
+                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+
+			// if the user is found but the password is wrong
+            if (!user.validPassword(password))
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+
+            // all is well, return successful user
+            return done(null, user);
         });
 
     }));
